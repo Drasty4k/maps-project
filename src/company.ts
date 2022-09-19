0a157d8011e2b265e6c8fbd668d793f2d9844a91
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-09-19 15:17:04 +0300

    use the Implements Clause to check the classes if they satisfy the Mappable interface

--- src/company.ts
+++ src/company.ts
@@ -1,12 +1,14 @@
 import { faker } from "@faker-js/faker";
+import { Mappable } from "./Map";
 
-export class Company {
+export class Company implements Mappable {
   name: string;
   catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = "blue";
 
   constructor() {
     this.name = faker.company.name();
